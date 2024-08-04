61744453dbb7078f5d2fb6cd14790f3a90123f25
nanasnumber <nanasnumber@gmail.com>
2024-08-04 14:45:09 -0400

    Add all book related routes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,34 +10,69 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
+
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).json(books)
 });
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+  const isbn = req.params.isbn;
+
+  if (books[isbn] === undefined) {
+    return res.status(300).json({message: "Book not found."});
+  }
+
+  return res.status(200).json(books[isbn]);
+});
+
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let matched = [];
+
+  for (const key in books) {
+    if (books[key].author === author) {
+      matched.push(books[key])
+    }
+  }
+
+  return res.status(200).json({
+    booksbyauthor: matched
+  });
 });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  let matched = [];
+
+  for (const key in books) {
+    if (books[key].title === title) {
+      matched.push(books[key])
+    }
+  }
+
+  return res.status(200).json({
+    booksbytitle: matched
+  });
 });
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+
+  if (books[isbn] === undefined) {
+    return res.status(300).json({message: "Book not found."});
+  }
+
+  return res.status(200).json(books[isbn].reviews);
 });
+
 
 module.exports.general = public_users;
